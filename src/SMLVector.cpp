a1e007ce0cbd7ac32727643bb1bd0ef4c90b5c0a
Oyale <salamivictor15@gmail.com>
2023-02-26 23:31:10 +0100

    Changed Vector definition to a template Vec definition

--- src/SMLVector.cpp
+++ src/SMLVector.cpp
@@ -1,257 +1,179 @@
 #include "SMLVector.h"
 
-#pragma region Vector2
-
-SML::Vector2::Vector2(const Vector2& a)
+namespace SML
 {
-	x = a.x;
-	y = a.y;
+	#pragma region Vec2
+	template<class T>
+	Vec2<T>::Vec2(const Vec2<T>& a)
+	{
+		x = a.x;
+		y = a.y;
+	}
+	
+	template<class T>
+	SML::Vec2<T>::Vec2(const T& X, const T& Y)
+	{
+		x = X;
+		y = Y;
+	}
+
+	template<class T>
+	float Vec2<T>::magnitude() const
+	{
+		return sqrt(double(x * x) + double(y * y));
+	}
+
+	template<class T>
+	float Vec2<T>::magnitudeSqr() const
+	{
+		return ((x * x) + (y * y));
+	}
+
+	template<class T>
+	float Vec2<T>::dot(const Vec2& a) const
+	{
+		return ((x * a.x) + (y * a.y));
+	}
+
+	template<class T>
+	Vec2<T> Vec2<T>::normalized() const
+	{
+		return Vec2(*this / Length());
+	}
+
+	template<class T>
+	Vec2<T> Vec2<T>::operator+(const Vec2& a) const
+	{
+		return Vec2(x + a.x, y + a.y);
+	}
+
+	template<class T>
+	Vec2<T> Vec2<T>::operator-(const Vec2& a) const
+	{
+		return Vec2(x - a.x, y - a.y);
+	}
+
+	template<class T>
+	Vec2<T> Vec2<T>::operator/(const float& a) const
+	{
+		return Vec2(x / a, y / a);
+	}
+
+	template<class T>
+	Vec2<T> Vec2<T>::operator*(const float& a) const
+	{
+		return Vec2(x * a, y * a);
+	}
+
+	template<class T>
+	Vec2<T> Vec2<T>::Scale(const float& a, const float& b) const
+	{
+		return Vec2(x * a, y * b);
+	}
+	#pragma endregion
+
+	#pragma region Vec3
+
+	template<class T>
+	Vec3<T>::Vec3(const Vec3& a)
+	{
+		x = a.x;
+		y = a.y;
+		z = a.z;
+	}
+
+	template<class T>
+	Vec3<T>::Vec3(const T& X, const T& Y, const T& Z)
+	{
+		x = X;
+		y = Y;
+		z = Z;
+	}
+
+	template<class T>
+	float Vec3<T>::magnitude() const
+	{
+		return sqrt((x * x) + (y * y) + (z * z));
+	}
+
+	template<class T>
+	float Vec3<T>::magnitudeSqr() const
+	{
+		return (x * x) + (y * y) + (z * z);
+	}
+
+	template<class T>
+	float Vec3<T>::dot(const Vec3& a) const
+	{
+		return ((x * a.x) + (y * a.y) + (z * a.z));
+	}
+
+	template<class T>
+	Vec3<T> Vec3<T>::normalized() const
+	{
+		return Vec3(*this / LengthSqr());
+	}
+
+	template<class T>
+	Vec3<T> Vec3<T>::operator+(const Vec3& a) const
+	{
+		Vec3 result;
+		_declspec(align(16)) Vec3 X(x, y, z);
+		_declspec(align(16)) Vec3 Y(a.x, a.y, a.z);
+
+		_mm_store_ps(&result.x, _mm_add_ps(_mm_load_ps(&X.x), _mm_load_ps(&Y.x)));
+
+		return result;
+	}
+
+	template<class T>
+	Vec3<T> Vec3<T>::operator-(const Vec3& a) const
+	{
+		Vec3 result;
+		_declspec(align(16)) Vec3 X(x, y, z);
+		_declspec(align(16)) Vec3 Y(a.x, a.y, a.z);
+
+		_mm_store_ps(&result.x, _mm_sub_ps(_mm_load_ps(&X.x), _mm_load_ps(&Y.x)));
+
+		return result;
+	}
+
+	template<class T>
+	Vec3<T> Vec3<T>::operator/(const float& a) const
+	{
+		return Vec3(x / a, y / a, z / a);
+	}
+
+	template<class T>
+	Vec3<T> Vec3<T>::operator*(const float& a) const
+	{
+		return Vec3(x * a, y * a, z * a);
+	}
+
+	template<class T>
+	Vec3<T> Vec3<T>::operator*(const Mat3x3& a) const
+	{
+		return Vec3(
+			((x * a.m[0][0]) + (y * a.m[1][0]) + (z * a.m[2][0])),
+			((x * a.m[0][1]) + (y * a.m[1][1]) + (z * a.m[2][1])),
+			((x * a.m[0][2]) + (y * a.m[1][2]) + (z * a.m[2][2])));
+	}
+
+
+	template<class T>
+	Vec3<T> Vec3<T>::scale(const float& a, const float& b, const float& c) const
+	{
+		return Vec3(a * x, b * y, c * z);
+	}
+
+	template<class T>
+	Vec3<T> Vec3<T>::cross(const Vec3& a) const
+	{
+		return Vec3((y * a.z) - (z * a.y),
+			(z * a.x) - (x * a.z),
+			(x * a.y) - (y * a.x));
+	}
+
+	#pragma endregion
+
 }
-
-SML::Vector2::Vector2(const float& X, const float& Y)
-{
-	x = X;
-	y = Y;
-}
-
-float SML::Vector2::Length() const
-{
-	return sqrt( double(x*x) + double(y*y) );
-}
-
-float SML::Vector2::LengthSqr() const
-{
-	return ( (x*x) + (y*y) );
-}
-
-float SML::Vector2::dot(const Vector2& a) const
-{
-	return ( (x*a.x) + (y*a.y) );
-}
-
-SML::Vector2 SML::Vector2::Normalized() const
-{
-	return Vector2(*this/Length());
-}
-
-SML::Vector2 SML::Vector2::Cross(const Vector2& a) const
-{
-	//TODO: Cross-Product of a Vector2
-	return Vector2();
-}
-
-SML::Vector2 SML::Vector2::operator+(const Vector2& a) const
-{
-	return Vector2(x + a.x, y + a.y);
-}
-
-SML::Vector2 SML::Vector2::operator-(const Vector2& a) const
-{
-	return Vector2(x - a.x, y - a.y);
-}
-
-SML::Vector2 SML::Vector2::operator/(const float& a) const
-{
-	return Vector2(x/a, y/a);
-}
-
-SML::Vector2 SML::Vector2::operator*(const float& a) const
-{
-	return Vector2(x*a, y*a);
-}
-
-SML::Vector2 SML::Vector2::Scale(const float& a, const float& b) const
-{
-	return Vector2(x*a, y*b);
-}
-
-
-#pragma endregion
-
-#pragma region Point3
-
-SML::Point3::Point3(const Point3& a)
-{
-	x = a.x;
-	y = a.y;
-	z = a.z;
-}
-
-SML::Point3::Point3(const float& X, const float& Y, const float& Z)
-{
-	x = X;
-	y = Y;
-	z = Z;
-}
-
-SML::Vector3 SML::Point3::operator-(const Point3& a) const
-{
-	Vector3 result;
-	_declspec(align(16)) Point3 X(x, y, z);
-	_declspec(align(16)) Point3 Y(a.x, a.y, a.z);
-
-	_mm_store_ps(&result.x, _mm_sub_ps(_mm_load_ps(&X.x), _mm_load_ps(&Y.x)));
-
-	return result;
-}
-
-SML::Point3 SML::Point3::operator+(const Vector3& a) const
-{
-	Point3 result;
-	_declspec(align(16)) Point3 X(x, y, z);
-	_declspec(align(16)) Point3 Y(a.x, a.y, a.z);
-
-	_mm_store_ps(&result.x, _mm_add_ps(_mm_load_ps(&X.x), _mm_load_ps(&Y.x)));
-
-	return result;
-}
-
-float SML::Point3::Length() const
-{
-	return sqrt((x * x) + (y * y) + (z * z));
-}
-
-float SML::Point3::LengthSqr() const
-{
-	return (x * x) + (y * y) + (z * z);
-}
-
-float SML::Point3::dot(const Vector3& a) const
-{
-	return ((x * a.x) + (y * a.y) + (z * a.z));
-}
-
-float SML::Point3::dot(const Point3& a) const
-{
-	return ((x * a.x) + (y * a.y) + (z * a.z));
-}
-
-SML::Point3 SML::Point3::Normalized() const
-{
-	Point3 normalized;
-	normalized = *this / LengthSqr();
-	return normalized;
-}
-
-SML::Point3 SML::Point3::operator/(const float& a) const
-{
-	return Point3(x / a, y / a, z / a);
-}
-
-SML::Point3 SML::Point3::operator*(const float& a) const
-{
-	return Point3(x * a, y * a, z * a);
-}
-
-SML::Point3 SML::Point3::operator*(const Mat3x3& a) const
-{
-	return Point3(
-		((x * a.m[0][0]) + (y * a.m[1][0]) + (z * a.m[2][0])),
-		((x * a.m[0][1]) + (y * a.m[1][1]) + (z * a.m[2][1])),
-		((x * a.m[0][2]) + (y * a.m[1][2]) + (z * a.m[2][2]))
-	);
-}
-
-SML::Point3 SML::Point3::Cross(const Point3& a) const
-{
-	Point3 Cross;
-	Cross.x = (y * a.z) - (z * a.y);
-	Cross.y = (z * a.x) - (x * a.z);
-	Cross.z = (x * a.y) - (y * a.x);
-	return Cross;
-}
-
-#pragma endregion
-
-#pragma region Vector3
-
-SML::Vector3::Vector3(const Vector3& a)
-{
-	x = a.x;
-	y = a.y;
-	z = a.z;
-}
-
-SML::Vector3::Vector3(const float& X, const float& Y, const float& Z)
-{
-	x = X;
-	y = Y;
-	z = Z;
-}
-
-float SML::Vector3::Length() const
-{
-	return sqrt((x * x) + (y * y) + (z * z));
-}
-
-float SML::Vector3::LengthSqr() const
-{
-	return (x * x) + (y * y) + (z * z);
-}
-
-float SML::Vector3::dot(const Vector3& a) const
-{
-	return ((x * a.x) + (y * a.y) + (z * a.z));
-}
-
-SML::Vector3 SML::Vector3::Normalized() const
-{
-	return Vector3(*this / LengthSqr());
-}
-
-SML::Vector3 SML::Vector3::operator+(const Vector3& a) const
-{
-	Vector3 result;
-	_declspec(align(16)) Vector3 X(x, y, z);
-	_declspec(align(16)) Vector3 Y(a.x, a.y, a.z);
-
-	_mm_store_ps(&result.x, _mm_add_ps(_mm_load_ps(&X.x), _mm_load_ps(&Y.x)));
-
-	return result;
-}
-
-SML::Vector3 SML::Vector3::operator-(const Vector3& a) const
-{
-	Vector3 result;
-	_declspec(align(16)) Vector3 X(x, y, z);
-	_declspec(align(16)) Vector3 Y(a.x, a.y, a.z);
-
-	_mm_store_ps(&result.x, _mm_sub_ps(_mm_load_ps(&X.x), _mm_load_ps(&Y.x)));
-
-	return result;
-}
-
-SML::Vector3 SML::Vector3::operator/(const float& a) const
-{
-	return Vector3(x / a, y / a, z / a);
-}
-
-SML::Vector3 SML::Vector3::operator*(const float& a) const
-{
-	return Vector3(x * a, y * a, z * a);
-}
-
-SML::Vector3 SML::Vector3::operator*(const Mat3x3& a) const
-{
-	return Vector3(
-		((x * a.m[0][0]) + (y * a.m[1][0]) + (z * a.m[2][0])),
-		((x * a.m[0][1]) + (y * a.m[1][1]) + (z * a.m[2][1])),
-		((x * a.m[0][2]) + (y * a.m[1][2]) + (z * a.m[2][2])));
-}
-
-
-SML::Vector3 SML::Vector3::Scale(const float& a, const float& b, const float& c) const
-{
-	return Vector3(a*x, b*y, c*z);
-}
-
-SML::Vector3 SML::Vector3::Cross(const Vector3& a) const
-{
-	return Vector3( (y * a.z) - (z * a.y),
-					(z * a.x) - (x * a.z),
-					(x * a.y) - (y * a.x) );
-}
-
-#pragma endregion
-
-
--- src/SMLVector.h
+++ src/SMLVector.h
@@ -1,89 +1,88 @@
 #pragma once
 #include <math.h>
-#include <string>
 #include <mmintrin.h>
 #include <xmmintrin.h>
 #include "SMLMat3x3.h"
 
 namespace SML
 {
-	class Vector3
+	template<class T>
+	class Vec3
 	{
 	public:
-		Vector3() {}
-		Vector3(const Vector3& a);
-		Vector3(const float& X, const float& Y, const float& Z);
+		/// <summary>Default constructor, everything is zeroed</summary>
+		Vec3() 
+		{
+			x = T(0);
+			y = T(0);
+			z = T(0);
+		}
+		/// <summary>Constructs a vector from another referenced one</summary>
+		Vec3(const Vec3& a);
+		/// <summary>Constructs a vector from individual values</summary>
+		Vec3(const T& X, const T& Y, const T& Z);
 
-		float x = 0, y = 0, z = 0;
+		T x, y, z;
+		/// <returns>The magnitude of vector</returns>
+		float magnitude() const;
+		/// <returns>The square of the magnitude of vector</returns>
+		float magnitudeSqr() const;
+		/// <summary>Returns the dot product of the vector with a</summary>
+		float dot(const Vec3& a) const;
 
-		float Length() const;
-		float LengthSqr() const;
-		float dot(const Vector3& a) const;
+		/// <returns>A normalized form of the vector</returns>
+		Vec3 normalized() const;
+		/// <summary>Returns the cross product of the vector with a</summary>
+		Vec3 cross(const Vec3& a) const;
 
-		Vector3 Normalized() const;
-		Vector3 Cross(const Vector3& a) const;
+		Vec3 operator+(const Vec3& a) const;
+		Vec3 operator-(const Vec3& a) const;
 
-		Vector3 operator+(const Vector3& a) const;
-		Vector3 operator-(const Vector3& a) const;
+		Vec3 operator/(const float& a) const;
+		Vec3 operator*(const float& a) const;
 
-		Vector3 operator/(const float& a) const;
-		Vector3 operator*(const float& a) const;
+		Vec3 operator*(const Mat3x3& a) const;
 
-		Vector3 operator*(const Mat3x3& a) const;
-
-		Vector3 Scale(const float& a, const float& b, const float& c) const;
+		Vec3 scale(const float& a, const float& b, const float& c) const;
 	};
 
-	class Vector2
+	template<class T>
+	class Vec2
 	{
 	public:
-		Vector2() {}
-		Vector2(const Vector2& a);
-		Vector2(const float& X, const float& Y);
+		Vec2() {}
+		Vec2(const Vec2<T>& a);
+		Vec2(const T& X, const T& Y);
 
-		float x = 0, y = 0;
+		T x, y;
 
-		float Length() const;
-		float LengthSqr() const;
-		float dot(const Vector2& a) const;
+		float magnitude() const;
+		float magnitudeSqr() const;
+		float dot(const Vec2& a) const;
 
-		Vector2 Normalized() const;
-		Vector2 Cross(const Vector2& a) const;
+		Vec2 normalized() const;
 
-		Vector2 operator+(const Vector2& a) const;
-		Vector2 operator-(const Vector2& a) const;
+		Vec2 operator+(const Vec2& a) const;
+		Vec2 operator-(const Vec2& a) const;
 
-		Vector2 operator/(const float& a) const;
-		Vector2 operator*(const float& a) const;
+		Vec2 operator/(const int& a) const;
+		Vec2 operator/(const float& a) const;
 
-		Vector2 operator*(const Mat3x3& a) const;
+		Vec2 operator*(const int& a) const;
+		Vec2 operator*(const float& a) const;
 
-		Vector2 Scale(const float& a, const float& b) const;
+		Vec2 operator*(const Mat3x3& a) const;
+
+		Vec2 Scale(const float& a, const float& b) const;
 	};
 
-	class Point3
-	{
-	public:
-		Point3() {}
-		Point3(const Point3& a);
-		Point3(const float& X, const float& Y, const float& Z);
+	#pragma region Typdefs
+	typedef Vec3<int> Vec3i;
+	typedef Vec3<float> Vec3f;
+	typedef Vec3<double> Vec3d;
 
-		float x = 0, y = 0, z = 0;
-
-		Vector3 operator-(const Point3& a) const;
-		Point3 operator+(const Vector3& a) const;
-
-		float Length() const;
-		float LengthSqr() const;
-		float dot(const Point3& a) const;
-		float dot(const Vector3& a) const;
-
-		Point3 Normalized() const;
-		Point3 Cross(const Point3& a) const;
-
-		Point3 operator/(const float& a) const;
-		Point3 operator*(const float& a) const;
-
-		Point3 operator*(const Mat3x3& a) const;
-	};
+	typedef Vec2<int> Vec2i;
+	typedef Vec2<float> Vec2f;
+	typedef Vec2<double> Vec2d;
+	#pragma endregion
 }
